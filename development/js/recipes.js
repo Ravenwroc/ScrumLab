b0b49f61ac844a44fef1f4f2ff0566d513add9ea
Marcin Guzek <36891603+Ravenwroc@users.noreply.github.com>
2019-05-29 10:18:38 +0200

    Update recipes.js
    Added recipe code to recipe.js

--- development/js/recipes.js
+++ development/js/recipes.js
@@ -33,3 +33,18 @@ userInfo.addEventListener("click", function () {
 
 })
 checkName();
+
+
+    let btnAll = document.querySelectorAll(".btn__trash");
+    console.log(btnAll);
+    let tableList = document.querySelector(".recipes__content");
+    console.log(tableList);
+
+    btnAll.forEach(function(element) {
+
+        element.addEventListener("click", function () {
+
+            tableList.removeChild(this.parentElement.parentElement);
+
+        })
+    })
