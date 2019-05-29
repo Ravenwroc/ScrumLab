0bbe11b72ae2c6a7343ba39a978ff01091adafaf
Filip <filszab@gmail.com>
2019-05-30 00:26:17 +0200

    task 6.1 test

--- development/js/addPlan.js
+++ development/js/addPlan.js
@@ -13,14 +13,14 @@ savePlanBtn.addEventListener("click", function storeName() {
     let titlePlan = document.querySelector(".plan_title").value;
     let descriptionPlan = document.querySelector(".plan_description").value;
     let weekPlan = document.querySelector(".inp_week").value;
-    let dishPlan = document.querySelector("select");
+    let dishPlan = document.querySelector("select").value;
 
     if (titlePlan.length > 0 && descriptionPlan.length > 0 && weekPlan.length > 0) {
         addContainer.style.display = "none";
         newPlan.title = titlePlan;
         newPlan.description = descriptionPlan;
         newPlan.number = weekPlan;
-        newPlan.dishes = dishPlan.value;
+        newPlan.dishes = dishPlan;
         saveRecipeToLocalStorage(newPlan);
 
     } else if (titlePlan.length == 0 && localStorage.getItem("title") &&
@@ -33,7 +33,7 @@ savePlanBtn.addEventListener("click", function storeName() {
 function saveRecipeToLocalStorage(newObject) {
   var dataFromLocalStorage = [];
   if (localStorage.getItem("plans") != null) {
-    dataFromLocalStorage = JSON.parse(localStorage.getItem("recipes"));
+    dataFromLocalStorage = JSON.parse(localStorage.getItem("plans"));
     dataFromLocalStorage.push(newObject);
     localStorage.setItem("plans", JSON.stringify(dataFromLocalStorage));
   } else {
