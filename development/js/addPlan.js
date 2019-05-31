f83d229d171c09ac8fe271bf1d0553e44c6df06b
Marcin Guzek <ravenwroc@interia.pl>
2019-05-31 13:09:26 +0200

    Add numbers to arrays in object "plans". Sort numbers in "plans" and "weeks" by ascending

--- development/js/addPlan.js
+++ development/js/addPlan.js
@@ -4,22 +4,22 @@ let addBtn = document.querySelector(".btn_addPlan");
 let mainView = document.querySelector(".dashboard");
 let weekPlan = document.querySelector(".inp_week").value;
 
-addBtn.addEventListener("click", function(){
-  let addPlanView = document.querySelector(".addplan__container");
+addBtn.addEventListener("click", function () {
+    let addPlanView = document.querySelector(".addplan__container");
 
-  addPlanView.style.display = "block";
-  mainView.style.display = "none";
+    addPlanView.style.display = "block";
+    mainView.style.display = "none";
 })
 
 let newPlan = {
-  title: "",
-  description: "",
-  // number: "",
-  dishes: []
+    number: "",
+    title: "",
+    description: "",
+    dishes: []
 };
 
 let numberWeek = {
-  number: ""
+    number: ""
 }
 
 savePlanBtn.addEventListener("click", function storeName() {
@@ -33,67 +33,75 @@ savePlanBtn.addEventListener("click", function storeName() {
 
     if (titlePlan.length > 0 && descriptionPlan.length > 0 && weekPlan.length > 0) {
 
-            newPlan.title = titlePlan;
-            newPlan.description = descriptionPlan;
+        newPlan.number = number;
+        newPlan.title = titlePlan;
+        newPlan.description = descriptionPlan;
 
-            dishPlan.forEach(function(element){
-              newPlan.dishes.push(element.value);
-            })
 
-            if (number > 0 && number < 53) {
-              if(allWeek != null){
-                for (let i=0; i<allWeek.length; i++) {
-                    if(number == allWeek[i]){
+        dishPlan.forEach(function (element) {
+            newPlan.dishes.push(element.value);
+        })
+
+        if (number > 0 && number < 53) {
+            if (allWeek != null) {
+                for (let i = 0; i < allWeek.length; i++) {
+                    if (number == allWeek[i]) {
                         return alert("Ten tydzień był już zapisany");
-                    }else {
-                      numberWeek.number = weekPlan;
+                    } else {
+                        numberWeek.number = weekPlan;
                     }
                 }
-              }
-              addContainer.style.display = "none";
-              mainView.style.display = "block";
-
-              saveRecipeToLocalStorages(weekPlan);
-              saveRecipeToLocalStorage(newPlan);
-
-              // titlePlan = "";
-              // descriptionPlan = "";
-              // weekPlan = "";
-              location.reload();
-
-            } else {
-              alert("Numer musi mieścić się w przedziale od 1 do 52");
             }
+            addContainer.style.display = "none";
+            mainView.style.display = "block";
+
+            saveRecipeToLocalStorages(weekPlan);
+            saveRecipeToLocalStorage(newPlan);
+
+            // titlePlan = "";
+            // descriptionPlan = "";
+            // weekPlan = "";
+            location.reload();
+
+        } else {
+            alert("Numer musi mieścić się w przedziale od 1 do 52");
+        }
 
 
-
-    } else if (titlePlan.length == 0 || localStorage.getItem("title")!= null &&
-               descriptionPlan.length == 0 || localStorage.getItem("description")!= null &&
-               weekPlan.length == 0 || localStorage.getItem("week")!= null) {
+    } else if (titlePlan.length == 0 || localStorage.getItem("title") != null &&
+        descriptionPlan.length == 0 || localStorage.getItem("description") != null &&
+        weekPlan.length == 0 || localStorage.getItem("week") != null) {
         alert("Exist or invalid name");
     }
 })
 
 function saveRecipeToLocalStorage(newObject) {
-  var dataFromLocalStorage = [];
-  if (localStorage.getItem("plans") != null) {
-    dataFromLocalStorage = JSON.parse(localStorage.getItem("plans"));
-    dataFromLocalStorage.push(newObject);
-    localStorage.setItem("plans", JSON.stringify(dataFromLocalStorage));
-  } else {
-    dataFromLocalStorage.push(newObject);
-    localStorage.setItem("plans", JSON.stringify(dataFromLocalStorage));
-  }
+    var dataFromLocalStorage = [];
+    if (localStorage.getItem("plans") != null) {
+        dataFromLocalStorage = JSON.parse(localStorage.getItem("plans"));
+        dataFromLocalStorage.push(newObject);
+        dataFromLocalStorage.sort(function(a, b) {
+            return a.number - b.number;
+        });
+        localStorage.setItem("plans", JSON.stringify(dataFromLocalStorage));
+
+    } else {
+        dataFromLocalStorage.push(newObject);
+        localStorage.setItem("plans", JSON.stringify(dataFromLocalStorage));
+    }
 }
 
 function saveRecipeToLocalStorages(newObject) {
-  var dataFromLocalStorage = [];
-  if (localStorage.getItem("weeks") != null) {
-    dataFromLocalStorage = JSON.parse(localStorage.getItem("weeks"));
-    dataFromLocalStorage.push(newObject);
-    localStorage.setItem("weeks", JSON.stringify(dataFromLocalStorage));
-  } else {
-    dataFromLocalStorage.push(newObject);
-    localStorage.setItem("weeks", JSON.stringify(dataFromLocalStorage));
-  }
+    var dataFromLocalStorage = [];
+    if (localStorage.getItem("weeks") != null) {
+        dataFromLocalStorage = JSON.parse(localStorage.getItem("weeks"));
+        dataFromLocalStorage.push(newObject);
+        dataFromLocalStorage.sort(function(a, b) {
+            return a - b;
+        });
+        localStorage.setItem("weeks", JSON.stringify(dataFromLocalStorage));
+    } else {
+        dataFromLocalStorage.push(newObject);
+        localStorage.setItem("weeks", JSON.stringify(dataFromLocalStorage));
+    }
 }
